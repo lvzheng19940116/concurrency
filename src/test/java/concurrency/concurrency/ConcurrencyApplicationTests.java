4d73cd0e9ebb80400988dec00c773f281cd97881
lvzheng <lvzheng@samsundot.com>
2019-12-05 10:25:22 +0800

    init

---
+++ src/test/java/concurrency/concurrency/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package concurrency.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
